d20d6694d69b130cab2cee7a418b3fad5e915a80
dasrah <dasrah.dev@gmail.com>
2024-12-29 23:44:37 +0100

    ajout ombre navbar root

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -4,7 +4,7 @@ import Navbar from "@/app/components/Navbar";
 function Layout({ children } : Readonly<{ children: React.ReactNode }>) {
     return (
         <main>
-            <Navbar />
+            <Navbar className="shadow" />
             {children}
         </main>
     );
