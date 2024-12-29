c49e359d323b05f6e9a6e5099e7a4a54400f8d8a
dasrah <dasrah.dev@gmail.com>
2024-12-29 23:44:20 +0100

    modif layout dashboard

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Sidebar from "@/app/components/Sidebar";
+import Navbar from "@/app/components/Navbar";
 
 export default function Layout({children} : Readonly<{ children: React.ReactNode }>) {
     return (
-        <div className="flex">
-            <Sidebar />
+        <div className="h-screen overflow-hidden">
+            <nav className="h-full">
+                <Navbar className="border-b-2 border-b-white-100" />
+                <Sidebar />
+            </nav>
             {children}
         </div>
     );
