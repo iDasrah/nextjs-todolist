3f6f777af0587659578656699c56b4f85e17578a
dasrah <dasrah.dev@gmail.com>
2024-12-29 23:45:01 +0100

    page tasks

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,3 +1,16 @@
 export default function Page() {
-    return <div></div>;
+    const tasks = [
+        {
+            id: 1,
+            title: "Apprendre Next.js",
+            description: "Apprendre Next.js pour pouvoir créer des applications web modernes.",
+            status: "Pending",
+            createdAt: new Date("2024-27-12-12:00:00"),
+        }
+    ];
+
+    return <div>
+        <ul>
+        </ul>
+    </div>;
 }
