d51e3c493812ac3162740ede9a5b30f6e013c930
Dasrah <dasrah.dev@gmail.com>
2024-12-29 19:43:25 +0100

    composant navbar

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,45 @@
+import Link from 'next/link';
+import {auth, signIn, signOut} from "@/auth";
+
+export default async function Navbar () {
+    const session = await auth();
+
+    return (
+        <nav className="flex items-center justify-between p-5">
+            <div className="flex items-center space-x-4">
+                <Link href="/" className="hover-blue-500 text-2xl">
+                    ToDoLIST
+                </Link>
+            </div>
+            <div className="flex items-center space-x-4">
+                {session && session?.user ? (
+                    <>
+                        <Link href="/dashboard" className="hover-blue-500">
+                            Dashboard
+                        </Link>
+                        <form action={async () => {
+                            "use server";
+
+                            await signOut();
+                        }}>
+                            <button className="hover-blue-500">
+                                Logout
+                            </button>
+                        </form>
+                    </>
+                ) : (
+                    <form action={async () => {
+                        "use server";
+
+                        await signIn("github");
+                    }}>
+                        <button className="hover:text-blue-800">
+                            Login with GitHub
+                        </button>
+                    </form>
+                )}
+
+            </div>
+        </nav>
+    );
+}
